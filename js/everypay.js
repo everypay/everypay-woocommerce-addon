4fb13883be93686d6b3a3670ca0555dc1ca7793c
Konstantinos Krevatas <297mxq@gmail.com>
2016-01-15 18:31:23 +0200

    Dynamically load the button and trigger the click after submit

--- js/everypay.js
+++ js/everypay.js
@@ -1,17 +1,28 @@
 var $checkout_form;
+var EVERYPAY_OPC_BUTTON;
 
-function init_everypay() {
-    $checkout_form = jQuery('form[name="checkout"]');    
-    EverypayButton.jsonInit(EVERYPAY_OPC_BUTTON, $checkout_form);
-}
+function load_everypay() {
+    var loadButton = setInterval(function () {
+        try {
+            $checkout_form = jQuery('form[name="checkout"]');
+            EverypayButton.jsonInit(EVERYPAY_OPC_BUTTON, $checkout_form);
 
-open_everypay_button = function(){
-    $checkout_form.find('.everypay-button').trigger('click');
+            var triggerButton = setInterval(function () {
+                try {
+                    $checkout_form.find('.everypay-button').trigger('click');
+                    clearInterval(triggerButton);
+                } catch (err) {
+                }
+            });
+
+            clearInterval(loadButton);
+        } catch (err) {
+        }
+    }, 301);
 }
 
 handleCallback = function (message) {
-    $checkout_form.append('<input type="hidden" value="' + message.token + '" name="everypayToken">');    
+    $checkout_form.append('<input type="hidden" value="' + message.token + '" name="everypayToken">');
     $checkout_form.submit();
-    $checkout_form.prepend('<div class="woocommerce-info">Submitting form.Please wait...</a></div>');    
+    $checkout_form.prepend('<div class="woocommerce-info">Submitting form.Please wait...</a></div>');
 };
-
