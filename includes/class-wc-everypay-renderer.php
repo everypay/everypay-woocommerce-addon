f9cbb141cd9135a20d09221a4a283274e111a027
Stefanos Giannakis <stefanoscoder@gmail.com>
2025-05-12 09:57:53 +0300

    indentation fix

--- includes/class-wc-everypay-renderer.php
+++ includes/class-wc-everypay-renderer.php
@@ -32,7 +32,7 @@ class WC_Everypay_Renderer
 			'billing_address' => $billing_address,
 			'email' => $billing_email,
 			'phone' => $billing_phone,
-            'woocommerce_version' => defined('WC_VERSION') ? WC_VERSION : '',
+			'woocommerce_version' => defined('WC_VERSION') ? WC_VERSION : '',
 		);
 
 		if (!empty($_POST['tokenized-card'])) {
